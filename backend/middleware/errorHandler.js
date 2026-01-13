ab906cf09472aa14785a4569669d0619316f82ff
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-01-13 19:25:21 +0530

    Set up the errors handling middleware

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,49 @@
+const errorHandler = (err,req,res,next) => {
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Server Error';
+
+    // mongoose bad ObjectId
+    if(err.name === 'CastError'){
+        message = 'Resource not found';
+        statusCode = 404;
+    }
+
+    // Mongoose Duplicate key
+    if(err.code === 11000){
+        const field = Object.keys(err.keyValue)[0];
+        message = `${field} already exists`;
+        statusCode : 400;
+    }
+
+    // Mongoose validation error
+    if(err.name === 'ValidationError'){
+        message = Object.values(err.errors).map(val => val.message).json(', ');
+        statusCode = 400;
+    }
+
+    //Multer file size error
+    if(err.code === 'LIMIT_FILE_SIZE'){
+        message = 'File size exceeds the maximum limit of 10MB';
+        statusCode = 400;
+    }
+
+    //JWT errors
+    if(err.code === 'JsonWebTokenError'){
+        message = 'Invalid Token';
+        statusCode = 401;
+    }
+
+    console.error('Error : ',{
+        message = err.message,
+        stack : process.env.NODE_ENV === 'development' ? err.stack : undefined
+    });
+
+    res.status(statusCode).json({
+        success : false,
+        error : message,
+        statusCode,
+        ...err(process.env.NODE_ENV === 'development' && {stack : err.stack})
+    });
+};
+
+export default errorHandler;
