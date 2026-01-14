04aa47014bc65a235b061ab1ce3492c48303ab82
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-01-14 19:18:18 +0530

    Completed User Model

---
+++ backend/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : [true , 'Please Provide a username'],
+        unique : true,
+        trim : true,
+        minlength : [3 , 'Username must be at least 3 characters long'],
+    },
+    email:{
+        type : String,
+        required : [true , 'Please Provide an email'],
+        unique : true,
+        lowercase : true,
+        match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email'],
+    },
+    password : {
+        type : String,
+        required : [true , 'Please Provide a password'],
+        minlength : [6 , 'Password must be at least 6 characters long'],
+        select : false,
+    },
+    profileImage:{
+        type:String,
+        default:null
+    }
+},{
+    timestamps : true,
+});
+
+// Hashing the password before saving in database
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+});
+
+// Compare password methoding
+userSchema.methods.matchPassword = async function(enterdPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+};
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
